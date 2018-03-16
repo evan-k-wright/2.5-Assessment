d32a71abefa561bff6b566399e39e9ac2335ecfc
Evan <rideburtonsnow3@gmail.com>
2018-03-16 13:49:29 -0600

    added selectors

---
+++ sorces/selectors.js
@@ -0,0 +1,10 @@
+module.exports = {
+    //Evens and Odds 
+        inputField : 'input[class="inputLine"]',
+        splitButton : 'button[class="confirmationButton"]',
+        evens : 'span[name="evenReulsts"]',
+        odd : 'span[name="oddResults"]',
+
+        
+
+}
